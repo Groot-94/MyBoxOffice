3b8bd4f3ed547422be142c6e5d2932ec04c8a8c3
starting1231 <starting1231@naver.com>
2023-02-06 14:36:40 +0900

    create: 프로젝트 생성 및 초기설정

---
+++ MyBoxOffice/MyBoxOffice/Application/SceneDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  SceneDelegate.swift
+//  MyBoxOffice
+//
+//  Created by Groot on 2023/02/06.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let rootViewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {}
+
+    func sceneDidBecomeActive(_ scene: UIScene) {}
+
+    func sceneWillResignActive(_ scene: UIScene) {}
+
+    func sceneWillEnterForeground(_ scene: UIScene) {}
+
+    func sceneDidEnterBackground(_ scene: UIScene) {}
+}
+
---
+++ MyBoxOffice/MyBoxOffice/Scene/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  MyBoxOffice
+//
+//  Created by Groot on 2023/02/06.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
